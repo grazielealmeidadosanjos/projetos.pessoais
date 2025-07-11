1e92524f25a93bb42a2f3ed3393939789b6f7901
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-11 10:12:41 -0300

    Create strlesemusarelamesma.c

---
+++ strlesemusarelamesma.c
@@ -0,0 +1,21 @@
+int strlenn(char R[100]){
+    int n=0;
+    for(int i=0; i<100; i++){
+        if(R[i]== '\0'){//string sempre termina com /0
+            break;//para o programa porque chegou no fim da string
+        }
+        n++;
+    }
+    return n;
+}
+
+#include <stdio.h>
+int main()
+{
+    char R[100];
+    int n, x;
+    n=0;
+    scanf("%s", R);
+    printf("%d", strlenn(R));
+    return 0;
+}
