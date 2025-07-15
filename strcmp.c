815707128d9d6a0f3135f96b53d26c4650fa2ec0
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-15 14:25:34 -0300

    Update strcmp.c

--- strcmp.c
+++ strcmp.c
@@ -3,10 +3,10 @@
 int strcmpp(char R[100], char S[100]){
    for (int i = 0; R[i] != '\0' || S[i] != '\0'; i++) {
         if (R[i] != S[i]) {
-            return 0;
+            return 1;
         }
     }
-    return 1;
+    return 0;
 }
    
 
