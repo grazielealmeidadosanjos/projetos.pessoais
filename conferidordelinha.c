5ad458c52493d063b4987d9824a8f11bf4b3e8bf
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-15 17:40:14 -0300

    Update conferidordelinha.c

--- conferidordelinha.c
+++ conferidordelinha.c
@@ -1,28 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
     int matriz[9][9];
-    int quantidade[9]={0};
+    int quantidade[9] = {0};
     int w;
-    for(int i=0; i<9; i++){
-        for(int g=0; g<9;g++){
+    for(int i = 0; i < 9; i++){
+        for(int g = 0; g < 9; g++){
             scanf("%d", &matriz[i][g]);
         }
     }
-    
-    //conferir linha n tem nada repetido
-    for(int j=0; j<9; j++){
-        for(w=0; w<9; w++){
-        int numero = matriz [j][w];
-        quantidade[numero-1]++;
-        if(quantidade[numero-1]>1){
-            printf("linha inválida\n");
-            break;
+        for(int j = 0; j < 9; j++){
+        for(int k = 0; k < 9; k++){
+            quantidade[k] = 0;
         }
-        }
-        if(w == 8){
-                printf("linha válida\n");
+
+        for(w = 0; w < 9; w++){
+            int numero = matriz[j][w];
+            quantidade[numero - 1]++;
+            if(quantidade[numero - 1] > 1){
+                printf("linha inválida\n");
+                break;
             }
+        }
+        
+        if(w == 8){ 
+            printf("linha válida\n");
+        }
     }
-        return 0;
+
+    return 0;
 }
