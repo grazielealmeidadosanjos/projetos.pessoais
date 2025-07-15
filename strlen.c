6f5023533d0e373c301251af2295d220c760fff5
grazielealmeidadosanjos <grazi.almeida1405@gmail.com>
2025-07-15 14:29:31 -0300

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -1,5 +1,5 @@
 int strlenn(char R[100]){
-    int n=0;
+    int n=0;//quantidade
     for(int i=0; i<100; i++){
         if(R[i]== '\0'){//string sempre termina com /0
             break;//para o programa porque chegou no fim da string
